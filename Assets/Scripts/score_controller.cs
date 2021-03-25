611def30597c07bb5dea14d752b8b00084fb2abe
tvmmyxn <78470284+tvmmyxn@users.noreply.github.com>
2021-03-24 19:21:28 -0700

    Initial commit

---
+++ Assets/Scripts/score_controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score_controller : MonoBehaviour
+{
+
+    public int score = 0;
+
+    public void UpdateScore()
+    {
+        GetComponent<Text>().text = "Score: " + score; 
+    }
+}
